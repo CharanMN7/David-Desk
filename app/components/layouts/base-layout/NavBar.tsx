332a0df37790b578af0ecc4860b3b290fc996cc0
Charan <charanxmn@gmail.com>
2024-09-03 09:18:54 +0530

    Updated the Navbar with DavidDesk name

--- app/components/layouts/base-layout/NavBar.tsx
+++ app/components/layouts/base-layout/NavBar.tsx
@@ -3,7 +3,7 @@ import { Bell, CircleUserRound } from "lucide-react";
 export const Navbar = () => {
   return (
     <header className="navbar">
-      <h1>RCP IMS</h1>
+      <h1>🤖 David Desk</h1>
       <nav className="nav-btns">
         <a href="#">
           <Bell className="nav-icon" />
