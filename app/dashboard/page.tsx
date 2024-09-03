70f0a6b3481f011919508cdd538b362f77d78f31
Charan <charanxmn@gmail.com>
2024-09-03 09:09:01 +0530

    Created the Dashboard page

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { PageLayout } from "../components/layouts/page-layout/PageLayout";
+
+const Dashboard = () => {
+  return (
+    <PageLayout heading="Dashboard">
+      <></>
+    </PageLayout>
+  );
+};
+export default Dashboard;
