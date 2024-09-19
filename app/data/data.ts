3fcdf9f3e49d1a1e62912e52cb66de6affed4d30
Charan <charanxmn@gmail.com>
2024-09-19 14:11:54 +0530

    Create data.ts

---
+++ app/data/data.ts
@@ -0,0 +1,135 @@
+export interface DataType {
+  name: string;
+  roll_no: string;
+  attendance: number;
+  grade: number;
+}
+
+export const data: Array<DataType> = [
+  {
+    name: "Jack",
+    roll_no: "598",
+    attendance: 72,
+    grade: 78,
+  },
+  {
+    name: "Jill",
+    roll_no: "599",
+    attendance: 82,
+    grade: 88,
+  },
+  {
+    name: "John",
+    roll_no: "600",
+    attendance: 92,
+    grade: 98,
+  },
+  {
+    name: "Jane",
+    roll_no: "601",
+    attendance: 62,
+    grade: 68,
+  },
+  {
+    name: "James",
+    roll_no: "602",
+    attendance: 52,
+    grade: 58,
+  },
+  {
+    name: "Jenny",
+    roll_no: "603",
+    attendance: 42,
+    grade: 48,
+  },
+  {
+    name: "Jasmine",
+    roll_no: "604",
+    attendance: 32,
+    grade: 38,
+  },
+  {
+    name: "Jude",
+    roll_no: "605",
+    attendance: 22,
+    grade: 28,
+  },
+  {
+    name: "Jared",
+    roll_no: "606",
+    attendance: 12,
+    grade: 18,
+  },
+  {
+    name: "Jesse",
+    roll_no: "607",
+    attendance: 2,
+    grade: 8,
+  },
+];
+
+const data_dummy = {
+  "csm-a": [
+    {
+      name: "Jack",
+      roll_no: "598",
+      attendance: 72,
+      grade: 78,
+    },
+    {
+      name: "Jill",
+      roll_no: "599",
+      attendance: 82,
+      grade: 88,
+    },
+    {
+      name: "John",
+      roll_no: "600",
+      attendance: 92,
+      grade: 98,
+    },
+    {
+      name: "Jane",
+      roll_no: "601",
+      attendance: 62,
+      grade: 68,
+    },
+    {
+      name: "James",
+      roll_no: "602",
+      attendance: 52,
+      grade: 58,
+    },
+    {
+      name: "Jenny",
+      roll_no: "603",
+      attendance: 42,
+      grade: 48,
+    },
+    {
+      name: "Jasmine",
+      roll_no: "604",
+      attendance: 32,
+      grade: 38,
+    },
+    {
+      name: "Jude",
+      roll_no: "605",
+      attendance: 22,
+      grade: 28,
+    },
+    {
+      name: "Jared",
+      roll_no: "606",
+      attendance: 12,
+      grade: 18,
+    },
+    {
+      name: "Jesse",
+      roll_no: "607",
+      attendance: 2,
+      grade: 8,
+    },
+  ],
+  "csm-b": [],
+};
