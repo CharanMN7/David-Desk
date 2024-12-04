d08d8d61d52ca56e7a0f47ed217febd26858c68e
Charan <charanxmn@gmail.com>
2024-12-05 05:00:40 +0530

    Setup the Inventory page

---
+++ app/admin/inventory/page.tsx
@@ -0,0 +1,6 @@
+import { ContentLayout } from "@/components/admin-panel/content-layout";
+
+const Page = () => {
+  return <ContentLayout title="Inventory">Page</ContentLayout>;
+};
+export default Page;
