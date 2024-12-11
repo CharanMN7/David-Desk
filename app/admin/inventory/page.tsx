d2eb12efd1db12d519c87005e8b15daaa991030c
Charan <charanxmn@gmail.com>
2024-12-11 22:49:43 +0530

    Update page.tsx

--- app/admin/inventory/page.tsx
+++ app/admin/inventory/page.tsx
@@ -1,13 +1,11 @@
 import { ContentLayout } from "@/components/admin-panel/content-layout";
-import ManageInventory from "./manage-inventory";
 import InventoryDashboard from "./manage-inventory";
 
 const Page = () => {
   return (
-  <ContentLayout title="Inventory">
-      <InventoryDashboard/>
-  </ContentLayout>
+    <ContentLayout title="Inventory">
+      <InventoryDashboard />
+    </ContentLayout>
   );
-
 };
 export default Page;
