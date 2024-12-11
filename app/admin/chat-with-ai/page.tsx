19dfb05b94ad41c7e44b0c0af890d2ea9885299a
Charan <charanxmn@gmail.com>
2024-12-11 22:13:50 +0530

    Added the chat-with-ai pages

--- app/admin/chat-with-ai/page.tsx
+++ app/admin/chat-with-ai/page.tsx
@@ -1,7 +1,8 @@
+"use client";
 import { ContentLayout } from "@/components/admin-panel/content-layout";
 
 const Page = () => {
-  return <ContentLayout title="Chat with David">Page</ContentLayout>;
+  return <ContentLayout title="Chat With AI">Page</ContentLayout>;
 };
 
 export default Page;
---
+++ app/student/chat-with-ai/page.tsx
@@ -0,0 +1,7 @@
+import { ContentLayout } from "@/components/admin-panel/content-layout";
+
+const Page = () => {
+  return <ContentLayout title="Chat with AI">Page</ContentLayout>;
+};
+
+export default Page;
